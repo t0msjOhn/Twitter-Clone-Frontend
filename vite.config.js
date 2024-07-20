00ffa592be146e130cfd732127eef9fefaadb9d0
Toms John <tomsjohn20@gmail.com>
2024-07-20 14:34:49 +0530

    Firebase commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Manually split vendor libraries into separate chunks
+          vendor: ['react', 'react-dom'],
+          // You can add more custom chunks here if needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed (default is 500 kB)
+  },
+});
